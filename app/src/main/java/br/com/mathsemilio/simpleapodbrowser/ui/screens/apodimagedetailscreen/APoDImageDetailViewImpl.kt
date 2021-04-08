3da6b20f07602ab359ccac37e58c016ec68c5e7c
mathsemilio <mathsemilio@gmail.com>
2021-04-07 21:48:53 -0300

    Initial implementation of PhotoView library.

--- app/src/main/java/br/com/mathsemilio/simpleapodbrowser/ui/screens/apodimagedetailscreen/APoDImageDetailViewImpl.kt
+++ app/src/main/java/br/com/mathsemilio/simpleapodbrowser/ui/screens/apodimagedetailscreen/APoDImageDetailViewImpl.kt
@@ -3,20 +3,20 @@ package br.com.mathsemilio.simpleapodbrowser.ui.screens.apodimagedetailscreen
 import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.core.view.isVisible
 import br.com.mathsemilio.simpleapodbrowser.R
+import com.github.chrisbanes.photoview.PhotoView
 import com.google.android.material.appbar.MaterialToolbar
 
 class APoDImageDetailViewImpl(layoutInflater: LayoutInflater, container: ViewGroup?) :
     APoDImageDetailView() {
 
-    private var imageViewAPoDImageDetail: ImageView
+    private var imageViewAPoDImageDetail: PhotoView
     private var toolbarAPoDImageDetail: MaterialToolbar
 
     init {
         rootView = layoutInflater.inflate(R.layout.apod_image_detail_screen, container, false)
-        imageViewAPoDImageDetail = findViewById(R.id.image_view_apod_image_detail)
+        imageViewAPoDImageDetail = findViewById(R.id.photo_view_apod_image_detail)
         toolbarAPoDImageDetail = findViewById(R.id.material_toolbar_apod_image_detail)
         setToolbarNavigationIconOnClickListener()
     }
