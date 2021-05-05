e1e4781e5288a70e91125e103c4ed9765596b4ac
Matt Menezes <mattmenezes@mattmenezes.home>
2021-05-04 23:05:32 -0300

    Segregated FetchAPoDUseCase and created addtional use cases; Refactored GlideProvider; Added IO_PARALLELISM_PROPERTY_NAME property and configured its value to Int.MAX_VALUE; Project organiztion and code cleanup.

---
+++ app/src/main/java/br/com/mathsemilio/simpleapodbrowser/domain/usecase/apod/FetchAPoDBasedOnDateUseCase.kt
@@ -0,0 +1,4 @@
+package br.com.mathsemilio.simpleapodbrowser.domain.usecase.apod
+
+class FetchAPoDBasedOnDateUseCase {
+}
--- app/src/main/java/br/com/mathsemilio/simpleapodbrowser/domain/usecase/apod/FetchAPoDsUseCase.kt
+++ app/src/main/java/br/com/mathsemilio/simpleapodbrowser/domain/usecase/apod/FetchAPoDsUseCase.kt
@@ -15,58 +15,27 @@ limitations under the License.
  */
 package br.com.mathsemilio.simpleapodbrowser.domain.usecase.apod
 
-import br.com.mathsemilio.simpleapodbrowser.common.formatTimeInMillis
 import br.com.mathsemilio.simpleapodbrowser.common.getLastSevenDays
 import br.com.mathsemilio.simpleapodbrowser.common.observable.BaseObservable
 import br.com.mathsemilio.simpleapodbrowser.common.toAPoDList
-import br.com.mathsemilio.simpleapodbrowser.common.toAPoD
 import br.com.mathsemilio.simpleapodbrowser.domain.model.APoD
 import br.com.mathsemilio.simpleapodbrowser.domain.model.Result
 import br.com.mathsemilio.simpleapodbrowser.networking.endpoint.APoDEndpoint
 
-class FetchAPoDUseCase(private val aPoDEndpoint: APoDEndpoint) :
-    BaseObservable<FetchAPoDUseCase.Listener>() {
+class FetchAPoDsUseCase(private val aPoDEndpoint: APoDEndpoint) :
+    BaseObservable<FetchAPoDsUseCase.Listener>() {
 
     interface Listener {
         fun onFetchAPoDBasedOnDateRangeCompleted(apods: List<APoD>)
-        fun onFetchAPoDBasedOnDateCompleted(apod: APoD)
-        fun onFetchRandomAPoDCompleted(randomAPoD: APoD)
+
         fun onFetchAPoDError(errorMessage: String)
     }
 
     suspend fun fetchAPoDBasedOnDateRange() {
-        aPoDEndpoint.getAPoDsBasedOnDateRange(getLastSevenDays()).also { result ->
+        aPoDEndpoint.fetchAPoDsBasedOnDateRange(getLastSevenDays()).also { result ->
             when (result) {
                 is Result.Completed -> listeners.forEach { listener ->
                     listener.onFetchAPoDBasedOnDateRangeCompleted(result.data?.toAPoDList()!!)
-                }
-                is Result.Failed -> listeners.forEach { listener ->
-                    listener.onFetchAPoDError(result.error!!)
-                }
-            }
-        }
-    }
-
-    suspend fun fetchAPoDBasedOnDate(dateInMillis: Long) {
-        aPoDEndpoint.getAPoDsBasedOnDate(dateInMillis.formatTimeInMillis()).also { result ->
-            when (result) {
-                is Result.Completed -> listeners.forEach { listener ->
-                    listener.onFetchAPoDBasedOnDateCompleted(result.data?.toAPoD()!!)
-                }
-                is Result.Failed -> listeners.forEach { listener ->
-                    listener.onFetchAPoDError(result.error!!)
-                }
-            }
-        }
-    }
-
-    suspend fun fetchRandomAPoD() {
-        aPoDEndpoint.getRandomAPoD().also { result ->
-            when (result) {
-                is Result.Completed -> listeners.forEach { listener ->
-                    listener.onFetchRandomAPoDCompleted(
-                        result.data?.toAPoDList()?.first()!!
-                    )
                 }
                 is Result.Failed -> listeners.forEach { listener ->
                     listener.onFetchAPoDError(result.error!!)
